1a9bb865bda134fac802f3e3ada10a0560661eb4
The-X-ray-man <petouhoff@gmail.com>
2024-06-29 22:02:09 +0200

    Add project files.

---
+++ Team.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+
+        private readonly List<Person> _firstTeam;
+        private readonly List<Person> _reserveTeam;
+        
+        public string Name { get; }
+        public IReadOnlyCollection<Person> FirstTeam => _firstTeam.AsReadOnly();
+        public IReadOnlyCollection<Person> ReserveTeam => _reserveTeam.AsReadOnly();   
+
+        public Team(string name)
+        {
+            Name = name;
+            _firstTeam = new List<Person>();
+            _reserveTeam = new List<Person>();
+        }
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40) _firstTeam.Add(person);
+            else _reserveTeam.Add(person);
+        }
+        public override string ToString() =>
+        $"First team has {FirstTeam.Count} players.{Environment.NewLine}Reserve team has {ReserveTeam.Count} players.";
+    }
+}
